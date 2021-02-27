7e03085ed5516a67798568db7f3d7cd2935297d7
James Joshua Pennington <JoshOSU25@gmail.com>
2021-02-27 06:31:27 -0500

    Added homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create a Blog model
+
+
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	pub_title = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
+	#title
+#pub date
+#body
+#image
+
+# Add a blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# add to the admin
+
