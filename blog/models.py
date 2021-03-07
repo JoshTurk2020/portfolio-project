05eb346dd82938a56c7f17274ae26c192d5835d2
James Joshua Pennington <JoshOSU25@gmail.com>
2021-03-06 23:11:05 -0500

    updated the pub_date

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Blog(models.Model):
 	title = models.CharField(max_length=255)
-	pub_title = models.DateTimeField()
+	pub_date = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 	#title
